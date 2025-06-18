396c1e21e5853bacd19ff5893928e9eec8a0a2d7
Lucas P. Rogero <lucas.rogero@sou.inteli.edu.br>
2025-06-18 14:04:30 +0000

    feat: acrescenta arquivos

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const studentController = require('../controllers/studentController');
 const reportController = require('../controllers/reportController');
+const teacherController = require('../controllers/teacherController');
+// cria o const teacher 
 
 router.get('/', (req, res) => {
   res.redirect('/insert');
